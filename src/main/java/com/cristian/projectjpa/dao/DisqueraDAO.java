51df28fcf8fb78e43ab6e888f273096c950bba71
cristian659 <velascocristian309@gmail.com>
2025-01-24 01:40:30 -0500

    Nombre del Proyecto: project-JPA

---
+++ src/main/java/com/cristian/projectjpa/dao/DisqueraDAO.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.cristian.projectjpa.dao;
+
+import java.util.List;
+
+import com.cristian.projectjpa.entity.Disquera;
+
+/**
+ * @author velas
+ * Interface que genera el DAO para las transacciones del CRUD a la tabla de disquera
+ */
+public interface DisqueraDAO {
+
+	 void guardar(Disquera disquera);
+	 
+	 void actualizar(Disquera disquera);
+	 
+	 void eliminar(Long id);
+	 
+	 List<Disquera> consultar();
+	 
+	 Disquera consultarById(Long id);
+}
